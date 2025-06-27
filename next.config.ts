4859190ece58b110aff42356be5a241396a40070
Zaid Ahmed <zaidmeer214@gmail.com>
2025-06-27 12:37:16 +0000

    Images 1280/1040 size rkho or full hd images me new links provide krrrha

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
